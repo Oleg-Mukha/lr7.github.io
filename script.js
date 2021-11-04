b2af9331f3bcbd4be5212b0b9846421c09892690
Oleg-Mukha <73238987+Oleg-Mukha@users.noreply.github.com>
2021-11-04 12:08:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.body.innerHTML = `
 <div class="shadow">
   <input class="field" id="question" type="text" placeholder="задати питання">
-  <div class = "circle" onclick="RandAnswer();"><img class = "circleImg" src="img/rabbit.gif"></div>
-  <div class="ans" id="predict"></div>
+  <div class="circle" id="crcl"><img class = "circleImg" src="img/rabbit.gif"></div>
+  <div class="answer" id="predict"></div>
 </div>
 `
 const answers = [
@@ -12,15 +12,22 @@ const answers = [
 
 ];
 
-function RandAnswer(){
-    if (document.getElementById("question").value == ""){
+document.getElementById('crcl').onclick = function (){
+    let field = document.getElementById("question").value;
+    if (field == ""){
         alert ( "Заповніть, будь-ласка, вище зазначене поле" );
         document.getElementById("question").style.background = "#ed37379e";
         }
-        else{
+    else if (field.indexOf("?") == -1){
+        alert("Ви ввели не запитання))");
+      }
+    else{
         document.getElementById("question").style.background = "#3d49828d";
         document.getElementById("predict").style.animation = "text 5s ease";
         let random = Math.floor(Math.random() * answers.length);
         document.getElementById("predict").innerHTML = answers[random];
-}
+        let anim = document.getElementById('crcl');
+        anim.classList.add('shakeAnim');
+        setTimeout(function(){anim.classList.remove('shakeAnim');}, 1000);
+      }
 }
